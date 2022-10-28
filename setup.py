9c02f5eb76803f6e9c61e00c84ee7dbd9cef432e
PratyushMalik <pratyush0410@gmail.com>
2022-10-29 00:37:25 +0530

    initialize repo

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="malik",
+    author_email="pratyush0410@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
