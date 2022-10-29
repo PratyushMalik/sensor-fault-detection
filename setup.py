f370ef708ad6774363842bdb5366d8ad1bc6d894
PratyushMalik <pratyush0410@gmail.com>
2022-10-29 17:24:11 +0530

    project structure, mongodb connection

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+
+    
+    return requirement_list
+
+
 setup(
     name="sensor",
     version="0.0.1",
     author="malik",
     author_email="pratyush0410@gmail.com",
     packages=find_packages(),
-    install_requires=[]
+    install_requires=get_requirements()
 )
